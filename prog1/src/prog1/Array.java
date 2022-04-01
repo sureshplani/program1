00d995ff802ec6b63e926e9dd9bea420292ee20d
sureshpalani <suresh16palani@mail.com>
2022-04-01 22:10:21 +0530

    array position

---
+++ prog1/src/prog1/Array.java
@@ -0,0 +1,19 @@
+package prog1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,20,30,40,50,};
+		int key=10;
+	    int pos=0;
+		for(int i:arr) {
+			pos++;
+			if(i==key)
+			System.out.println("possion:"+pos);
+			}
+		}
+
+	}
+
+
